e337e452dbf2348a9214600f4b4ce6eaf778dbf1
Jawad <Jawad.alderany@gmail.com>
2025-11-03 22:04:43 +0300

    Initial commit - cleaned history

---
+++ js/gallery.js
@@ -0,0 +1,388 @@
+var fileName;
+var srcWidth;
+var srcHeight;
+var curpage = 1;
+var pagesNum = 6;
+var sliding = false;
+var click = true;
+var left = document.getElementById("left");
+var right = document.getElementById("right");
+var pagePrefix = "slide";
+var pageShift = 500;
+var transitionPrefix = "circle";
+var svg = true;
+
+document.getElementsByClassName("close_btn")[0].addEventListener("click", function () {
+    if (history.length >= 1)
+        history.back();
+    else
+        location.href = "../index.html";
+});
+
+setAlbum();
+
+
+function leftSlide() {
+    if (click) {
+        if (curpage == 1) curpage = pagesNum + 1;
+        //console.log("woek");
+        sliding = true;
+        curpage--;
+        svg = true;
+        click = false;
+        for (k = 1; k <= pagesNum; k++) {
+            var a1 = document.getElementById(pagePrefix + k);
+            a1.className += " tran";
+        }
+        setTimeout(() => {
+            move();
+        }, 200);
+        setTimeout(() => {
+            for (k = 1; k <= pagesNum; k++) {
+                var a1 = document.getElementById(pagePrefix + k);
+                a1.classList.remove("tran");
+            }
+        }, 1400);
+    }
+}
+
+function rightSlide() {
+    if (click) {
+        if (curpage == pagesNum) curpage = 0;
+        //console.log("woek");
+        sliding = true;
+        curpage++;
+        svg = false;
+        click = false;
+        for (k = 1; k <= pagesNum; k++) {
+            var a1 = document.getElementById(pagePrefix + k);
+            a1.className += " tran";
+        }
+        setTimeout(() => {
+            move();
+        }, 200);
+        setTimeout(() => {
+            for (k = 1; k <= pagesNum; k++) {
+                var a1 = document.getElementById(pagePrefix + k);
+                a1.classList.remove("tran");
+            }
+        }, 1400);
+    }
+}
+
+function move() {
+    if (sliding) {
+        sliding = false;
+        if (svg) {
+            for (j = 1; j <= 9; j++) {
+                var c = document.getElementById(transitionPrefix + j);
+                c.classList.remove("steap");
+                c.setAttribute("class", transitionPrefix + j + " streak");
+                //console.log("streak");
+            }
+        } else {
+            for (j = 10; j <= 18; j++) {
+                var c = document.getElementById(transitionPrefix + j);
+                c.classList.remove("steap");
+                c.setAttribute("class", transitionPrefix + j + " streak");
+                //console.log("streak");
+            }
+        }
+        setTimeout(() => {
+            for (i = 1; i <= pagesNum; i++) {
+                if (i == curpage) {
+                    var a = document.getElementById(pagePrefix + i);
+                    a.className += " up1";
+                } else {
+                    var b = document.getElementById(pagePrefix + i);
+                    b.classList.remove("up1");
+                }
+            }
+            sliding = true;
+        }, 600);
+        setTimeout(() => {
+            click = true;
+        }, 1700);
+
+        setTimeout(() => {
+            if (svg) {
+                for (j = 1; j <= 9; j++) {
+                    var c = document.getElementById(transitionPrefix + j);
+                    c.classList.remove("streak");
+                    c.setAttribute("class", transitionPrefix + j + " steap");
+                }
+            } else {
+                for (j = 10; j <= 18; j++) {
+                    var c = document.getElementById(transitionPrefix + j);
+                    c.classList.remove("streak");
+                    c.setAttribute("class", transitionPrefix + j + " steap");
+                }
+                sliding = true;
+            }
+        }, 850);
+        setTimeout(() => {
+            click = true;
+        }, 1700);
+
+        //change count & title
+        setTimeout(() => {
+            setCountPage();
+            setTitlePage();
+        }, 1000);
+    }
+}
+
+left.onmousedown = () => {
+    leftSlide();
+};
+
+right.onmousedown = () => {
+    rightSlide();
+};
+
+document.onkeydown = e => {
+    if (e.keyCode == 37) {
+        leftSlide();
+    } else if (e.keyCode == 39) {
+        rightSlide();
+    }
+};
+
+//for codepen header
+// setTimeout(() => {
+// 	rightSlide();
+// }, 500);
+
+function setCountPage() {
+    document.getElementsByClassName("counter")[0].innerHTML = curpage + "/" + pagesNum;
+}
+
+function setTitlePage() {
+    //document.getElementsByClassName("title")[0].innerHTML = "Title " + curpage;
+}
+
+//
+//config images and sizes
+//
+
+function setAlbum() {
+
+    try {
+        var page = location.href.split('/');
+        var album = page[page.length - 1].split('?')[1].split('=')[1];
+
+    } catch (e) {
+        location.href = '../index.html';
+    }
+
+    var sliderBackground = "black";
+    switch (album) {
+        case 'propertymanagement':
+            fileName = '../img/propertymanagement_';
+            pagesNum = 51;
+            srcWidth = 1366;
+            srcHeight = 649;
+            document.getElementsByClassName("counter")[0].style.color = "#757575";
+            break;
+        case 'sterilizationunit':
+            fileName = '../img/sterilizationunit_';
+            pagesNum = 20;
+            srcWidth = 709;
+            srcHeight = 447;
+            break;
+        case 'tenancyagreements':
+            fileName = '../img/tenancyagreements_';
+            pagesNum = 16;
+            srcWidth = 1366;
+            srcHeight = 768;
+            break;
+        case 'knowledge':
+            fileName = '../img/knowledge_';
+            pagesNum = 19;
+            srcWidth = 1360;
+            srcHeight = 768;
+            break;
+        case 'settings':
+            fileName = '../img/settings_';
+            pagesNum = 6;
+            srcWidth = 890;
+            srcHeight = 542;
+            document.getElementsByClassName("counter")[0].style.color = "#757575";
+            break;
+        case 'sukna':
+            fileName = '../img/sukna_';
+            pagesNum = 23;
+            srcWidth = 1366;
+            srcHeight = 2246;
+            break;
+        case 'marketing':
+            //fileName = '../img/marketing/_';
+            //imageNum = 0;
+            location.href = '../index.html';
+            break;
+        case 'housingmanagement':
+            fileName = '../img/housingmanagement_';
+            pagesNum = 25;
+            srcWidth = 1366;
+            srcHeight = 649;
+            break;
+        case 'studentmanagement':
+            fileName = '../img/studentmanagement_';
+            pagesNum = 6;
+            srcWidth = 1366;
+            srcHeight = 649;
+            break;
+        case 'employeemanagement':
+            fileName = '../img/employeemanagement_';
+            pagesNum = 5;
+            srcWidth = 1366;
+            srcHeight = 649;
+            break;
+        case 'account':
+            fileName = '../img/account_';
+            pagesNum = 4;
+            srcWidth = 1080;
+            srcHeight = 2400;
+            break;
+        case 'sls':
+            fileName = '../img/sls_';
+            pagesNum = 17;
+            srcWidth = 1482;
+            srcHeight = 743;
+            document.getElementsByClassName("counter")[0].style.color = "#FAFAFA";
+            sliderBackground = "#757575";
+            break;
+        case 'tasky':
+            fileName = '../img/Tasky_';
+            pagesNum = 14;
+            srcWidth = 1366;
+            srcHeight = 633;
+            document.getElementsByClassName("counter")[0].style.color = "#757575";
+            //sliderBackground = "#757575";
+            break;
+        case 'hr':
+            fileName = '../img/hr_';
+            pagesNum = 27;
+            srcWidth = 1920;
+            srcHeight = 1000;
+            document.getElementsByClassName("counter")[0].style.color = "#757575";
+            //sliderBackground = "#757575";
+            break;
+        case 'justhalal':
+            fileName = '../img/justhalal_';
+            pagesNum = 18;
+            srcWidth = 1656;
+            srcHeight = 3584;
+            document.getElementsByClassName("counter")[0].style.color = "#757575";
+            //sliderBackground = "#757575";
+            break;
+        case 'gymio':
+            fileName = '../img/gymio_';
+            pagesNum = 6;
+            srcWidth = 1366;
+            srcHeight = 633;
+            document.getElementsByClassName("counter")[0].style.color = "#757575";
+            //sliderBackground = "#757575";
+            break;
+        default:
+            location.href = '../index.html';
+            break;
+    }
+
+    setSvgSize();
+    setImages(sliderBackground);
+    setCountPage();
+    setTitlePage();
+}
+
+function setImages(sliderBackground) {
+
+    for (var i = 1; i <= pagesNum; i++) {
+        //create and add slide 'i'
+        const slide = document.createElement("div");
+        slide.id = "slide" + i;
+        slide.classList.add("slide-c");
+        slide.classList.add("slide" + i);
+        slide.style.background = sliderBackground;
+        if (i == 1)
+            slide.classList.add("up1");
+
+        //slide.innerHTML = "Title Center" + i;
+
+        //set image as background
+        //slide.style.setProperty('background-image', 'url("' + fileName + '' + i + '.png")');
+
+        //or create image elemetnt
+        const image = document.createElement('img');
+        image.setAttribute('src', fileName + '' + i + '.jpg');
+        image.classList.add("center");
+        slide.appendChild(image);
+
+        document.getElementsByClassName("slider")[0].appendChild(slide);
+    }
+}
+
+function setSvgSize() {
+    // set parent ratio
+    var parent = document.getElementsByClassName("parent")[0];
+    var v = calculateAspectRatioFit(srcWidth, srcHeight, parent.offsetWidth, parent.offsetHeight);
+    //parent.setAttribute('aspect-ratio', 'v.heigh/v.width');
+    parent.style.height = v.height + "px";
+    parent.style.width = v.width + "px";
+
+    //set right svg start position
+    var svg1 = document.getElementById("svg1");
+    svg1.style.width = parent.offsetWidth + "px";
+    svg1.style.height = parent.offsetHeight + "px";
+
+    //set left svg start position
+    var svg2 = document.getElementById("svg2");
+    svg2.style.width = parent.offsetWidth + "px";
+    svg2.style.height = parent.offsetHeight + "px";
+
+    //set radius and position for circales
+    var space = parent.offsetWidth / 8;
+    var radius = 35;
+    var svg1cCircles = [].slice.call(svg1.children);
+    var svg2cCircles = [].slice.call(svg2.children);
+
+    for (var i = 0; i < svg1cCircles.length; i++) {
+        svg1cCircles[i].setAttribute('r', radius);
+        svg2cCircles[i].setAttribute('r', radius);
+        radius += space;
+    }
+}
+
+function calculateAspectRatioFit(srcWidth, srcHeight, maxWidth, maxHeight) {
+
+    var ratio = Math.min(maxWidth / srcWidth, maxHeight / srcHeight);
+
+    return { width: srcWidth * ratio, height: srcHeight * ratio };
+}
+
+//--------------------------------------- Events ---------------------------------//
+// Function to handle resizing and adjust margin-left
+function handleResize() {
+    var parentElement = document.querySelector('.parent');
+    var rightElement = document.querySelector('.right');
+
+    if (parentElement && rightElement) {
+        var parentWidth = parentElement.offsetWidth;
+
+        if (parentWidth <= 150) {
+            rightElement.style.marginLeft = '19%';
+        } else if (parentWidth <= 400) {
+            rightElement.style.marginLeft = '21.5%';
+        } else if (parentWidth <= 1000) {
+            rightElement.style.marginLeft = '23.5%';
+        } else {
+            rightElement.style.marginLeft = '23.5%';
+        }
+    }
+}
+
+// Initial adjustment on page load
+handleResize();
+
+// Add event listener for window resize
+window.addEventListener('resize', handleResize);
